f918957c5f5d8bca7c7d6b78b10ca677048c071c
ManoumeMF <manoume2023@gmail.com>
2024-07-30 16:01:41 +0700

    Update index.blade.php

--- resources/views/admin/PengaturanDanKonfigurasi/status/index.blade.php
+++ resources/views/admin/PengaturanDanKonfigurasi/status/index.blade.php
@@ -88,7 +88,7 @@
                     const toast = new bootstrap.Toast(primarytoastDeleteSuccess)
                     toast.show()
 
-                    setTimeout("window.location='{{ route('JenisStatus.index') }}'", 2500);
+                    setTimeout("window.location='{{ route('Status.index') }}'", 2500);
                 }
             }
         });
