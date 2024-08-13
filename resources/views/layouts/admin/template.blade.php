881c8e6877864061d66f8990128587a577fd1da4
Samuel Volder <113781767+StillLearnSVN@users.noreply.github.com>
2024-08-13 13:27:27 +0700

    completing CRUD for LokasiObjekRetribusi's data

--- resources/views/layouts/admin/template.blade.php
+++ resources/views/layouts/admin/template.blade.php
@@ -796,6 +796,9 @@
                                     <a href="{{ route('JenisObjekRetribusi.index') }}" class="side-menu__item">Jenis Objek Retribusi</a>
                                 </li>
                                 <li class="slide">
+                                    <a href="{{ route('LokasiObjekRetribusi.index') }}" class="side-menu__item">Lokasi Objek Retribusi</a>
+                                </li>
+                                <li class="slide">
                                     <a href="{{ route('Status.index') }}" class="side-menu__item">Status</a>
                                 </li>
                             </ul>
